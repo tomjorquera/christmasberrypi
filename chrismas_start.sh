5738e21aa518810b606f5947967380fedf8d6be5
Tom Jorquera <tom@jorquera.net>
2023-12-18 18:25:55 +0100

    reduce led flickering

--- chrismas_start.sh
+++ chrismas_start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# use hardware backend for reduced flickering
+sudo pigpiod
+
 cd /home/tom
-sudo -u tom python3 /home/tom/chrismas.py &
+sudo GPIOZERO_PIN_FACTORY=pigpio -u tom python3 /home/tom/chrismas.py &
 sudo -u tom ssh -N -R \*:9876:localhost:9876 chrismasberrypi@home.jorquera.net
